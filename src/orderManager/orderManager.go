8d8dd672001644a1aa7d2a62259d6e826669f59b
Tonja Joseph <tonjaj@stud.ntnu.no>
2018-03-02 20:31:06 +0100

    Implement orderManager module

--- src/orderManager/orderManager.go
+++ src/orderManager/orderManager.go
@@ -2,6 +2,7 @@ package orderManager
 
 import(
 	"../elevio"
+	def "../definitions"
 )
 
 
@@ -9,3 +10,48 @@ type Order struct{
 	Button elevio.ButtonType
 	Floor int
 }
+
+func isQueueEmpty(requests []bool) bool {
+	for i := 0; i < def.NUM_FLOORS; i++ {
+		if requests[i] {
+			return false
+		}
+	}
+	return true
+}
+
+
+func isOrdersAbove(requests []bool, current_floor int) bool {
+	for i:= floor-1; i >= 0; i-- {
+		if requests[i] {
+			dir = elevio.MD_Down
+		}
+	}
+}
+
+func isOrdersBelow(requests []bool, current_floor int) bool {
+	for i:= floor-1; i >= 0; i-- {
+		if requests[i] {
+			dir = elevio.MD_Down
+		}
+	}
+}
+
+func ChooseDirection(requests []bool, current_floor int, current_direction elevio.Motor) {
+
+	if isQueueEmpty(requests) { return elevio.MD_Stop }
+
+	if current_direction = elevio.MD_Up {
+		if isOrdersAbove(requests, current_floor) {
+			return current_direction
+		}
+	} else if current_direction = elevio.MD_Down {
+		if isOrdersBelow(requests, current_floor) {
+			return current_direction
+		}
+	}
+
+	return -1*current_direction
+}
+
+
