414f06666b285d0549fb72358844f51b3295cd5d
atussa <atussak@stud.ntnu.no>
2018-03-05 22:31:15 +0100

    Start network init function

--- src/network/network.go
+++ src/network/network.go
@@ -4,7 +4,40 @@ import (
 	"bcast"
 	"fmt"
 	"time"
+	"../localip"
+	"../peers"
 )
+
+type ElevatorUpdate struct {
+	Requests [][] bool,
+	Last_floor int,
+	Direction elevio.MotorDirection,
+	State int,
+	ID string,
+}
+
+
+func Init(peer_update_ch chan peers.PeerUpdate, peer_tx_enable chan bool) {
+
+	var id string
+
+	if id == "" {
+		localIP, err := localip.LocalIP()
+		if err != nil {
+			fmt.Println(err)
+			localIP = "DISCONNECTED"
+		}
+		id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
+	}
+
+	go peers.Transmitter(15647, id, peer_tx_enable)
+	go peers.Receiver(15647, peer_update_ch)
+}
+
+
+
+
+
 
 const localPort = 20007
 
