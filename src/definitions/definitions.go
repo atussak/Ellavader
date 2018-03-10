d8fd067a68546d6dc19219e39fd80a9f3683f638
atussa <atussak@stud.ntnu.no>
2018-03-10 23:52:07 +0100

    Add const for maximum ID and bottom and top floor

--- src/definitions/definitions.go
+++ src/definitions/definitions.go
@@ -4,7 +4,11 @@ const NUM_FLOORS = 4
 const NUM_ELEVATORS = 3
 const NUM_BUTTON_TYPES = 3
 
+const BOTTOM_FLOOR = 0
+const TOP_FLOOR = NUM_FLOORS - 1
 
 const IDLE = 0
 const MOVING = 1
 const DOOR_OPEN = 2
+
+const MAXIMUM_ID = 99999
