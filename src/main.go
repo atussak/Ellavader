835ad13efbeea503c1e6f6e81d83125031756409
atussa <atussak@stud.ntnu.no>
2018-03-11 16:46:09 +0100

    Add and use channel for remote order

--- src/main.go
+++ src/main.go
@@ -42,6 +42,9 @@ func main(){
 
     elev_update_rx_ch := make(chan OM.ElevatorData, 100)
 
+    new_remote_order_tx_ch := make(chan elevio.Order)
+    new_remote_order_rx_ch := make(chan elevio.Order, 100)
+
     // Inits
 
     
@@ -49,28 +52,45 @@ func main(){
 
     // Variables
 
-    peer_port := 15647 //anything?
+    peer_port := 15647
+    assign_port := 15648
     
 
     // Goroutines
 
-    go elevio.PollButtons(ch.New_order_ch)
+    go elevio.PollCabButtons(ch.New_order_ch)
+    go elevio.PollHallButtons(new_remote_order_tx_ch)
     go elevio.PollFloorSensor(ch.Floor_reached_ch)
+
+
+        // Start FSM
 
     go fsm.DoorTimer(ch.Start_timer_ch, ch.Timeout_ch)
     go fsm.Run(ch)
+
     
+        // Elevator communication
+
     go peers.Transmitter(peer_port, ip, peer_tx_enable)
     go peers.Receiver(peer_port, peer_update_ch)
+
     go bcast.Transmitter(peer_port, ch.Elev_update_tx_ch)
     go bcast.Receiver(peer_port, elev_update_rx_ch)
+
+    go bcast.Transmitter(assign_port, new_remote_order_tx_ch)
+    go bcast.Receiver(assign_port, new_remote_order_rx_ch)
 
 
     for{
         select{
         case elev_update := <- elev_update_rx_ch:
             OM.UpdateElevatorDatabase(elev_update, ch.Elev_update_tx_ch)
-        }
-
+        
+        case new_remote_order := <- new_remote_order_rx_ch:
+            ID := OM.AssignOrderToElevator(new_remote_order)
+            if OM.Local_data.ID == ID {
+                ch.New_order_ch <- new_remote_order
+            }
+        }   
     }
 }
